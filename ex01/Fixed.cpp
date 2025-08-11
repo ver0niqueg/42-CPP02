41a7b5e7e923518d7684f04931a1c349f813083d
Veronique Galmiche <vgalmich@f4r5s8.clusters.42paris.fr>
2025-08-11 14:19:08 +0200

    ex01 OK

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 13:13:29 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/08/11 14:18:46 by vgalmich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed(void) : _rawBits(0)
+{
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->_rawBits = copy.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &other)
+{
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        this->_rawBits = other.getRawBits();
+    return (*this);
+}
+
+Fixed::Fixed(const int intValue)
+{
+    std::cout << "Int constructor called" << std::endl;
+    _rawBits = intValue << _fractionalBits; // decale l'entier vers la gauche de fractionalbits
+}
+
+Fixed::Fixed(const float floatValue)
+{
+    std::cout << "Float construction called" << std::endl;
+    this->_rawBits = roundf(floatValue * (1 << _fractionalBits));
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+float Fixed::toFloat(void) const
+{
+    return (this->_rawBits / float(1 << _fractionalBits));
+}
+
+int Fixed::toInt(void) const
+{
+    return (this->_rawBits >>  _fractionalBits);
+}
+
+int Fixed::getRawBits(void) const
+{
+    return (this->_rawBits);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    this->_rawBits = raw;
+}
+
+std::ostream &operator<<(std::ostream &o, const Fixed &value)
+{
+    o << value.toFloat(); // on affiche la valeur en float
+    return o; // on retourne le flux pour pouvoir chainer les <<
+}
+
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgalmich <vgalmich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/11 13:13:27 by vgalmich          #+#    #+#             */
+/*   Updated: 2025/08/11 14:14:37 by vgalmich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int _rawBits;
+        static const int _fractionalBits = 8;
+
+    public:
+        Fixed(); // constructeur par defaut
+        Fixed(const Fixed &copy); // constructeur de recopie
+        Fixed &operator=(const Fixed &other); // surcharge operateur d'affectation
+        Fixed(const int intValue);
+        Fixed(const float floatValue);
+        ~Fixed(); // destructeur
+
+        int getRawBits(void) const; // getter
+        void setRawBits(int const raw); // setter
+
+        float toFloat(void) const; // convertit la valeur en virgule fixe en nb a virgule flottante
+        int toInt(void) const; // convertit la valeur en virgule fixe en nb entier
+};
+
+std::ostream &operator<<(std::ostream &o, const Fixed &value);
+
+#endif
